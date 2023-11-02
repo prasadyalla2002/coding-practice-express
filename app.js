b108b4cc5536e712adffa14c4ae1fb874ed81b33
prasad yalla <pyalla50@gmail.com>
2023-11-02 07:19:23 +0000

    Authentication

---
+++ app.js
@@ -0,0 +1,229 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+///register new user
+
+app.post("/users/", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender, location) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}',
+          '${location}'
+        )`;
+    await db.run(createUserQuery);
+    response.send(`Created new user with`);
+  } else {
+    response.status = 400;
+    response.send("User already exists");
+  }
+});
+
+///Login user
+
+app.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payLoad = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payLoad, "my_sec_code");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+/// Token Authentication
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid Access Token");
+  } else {
+    jwt.verify(jwtToken, "my_sec_code", async (error, payload) => {
+      if (error) {
+        response.send("Invalid Access Token");
+      } else {
+        next();
+      }
+    });
+  }
+};
+
+///Get Stated
+
+app.get("/states/", authenticateToken, async (request, response) => {
+  const getStatesQuery = `
+    SELECT *
+    FROM
+        state`;
+  const dbResponse = await db.all(getStatesQuery);
+  response.send(dbResponse);
+});
+
+/// Get state by stateId
+
+app.get("/states/:stateId/", authenticateToken, async (request, response) => {
+  const { stateId } = request.params;
+  const getStateQuery = `
+    SELECT *
+    FROM state
+    WHERE state_id = ${stateId};`;
+  const dbResponse = await db.get(getStateQuery);
+  response.send(dbResponse);
+});
+
+/// Post A district
+
+app.post("/districts/", authenticateToken, async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const addDistrictQuery = `
+    INSERT INTO
+        district(district_name, state_id, cases, cured, active, deaths)
+    VALUES
+        (
+        '${districtName}',
+        ${stateId},
+        ${cases},
+        ${cured},
+        ${active},
+        ${deaths}
+        )`;
+  await db.run(addDistrictQuery);
+  response.send("District Successfully Added");
+});
+
+///Get district by Id
+
+app.get(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const getDistrictQuery = `
+    SELECT * FROM district WHERE district_id = ${districtId};`;
+    const dbResponse = await db.get(getDistrictQuery);
+    response.send(dbResponse);
+  }
+);
+
+///Delete district
+
+app.delete(
+  "/districts/:districtId",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const deleteDistrictQuery = `
+    DELETE 
+    FROM district
+    WHERE district_id = ${districtId};`;
+    await db.run(deleteDistrictQuery);
+    response.send("District Removed");
+  }
+);
+
+///update district
+
+app.put(
+  "/districts/:districtId",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const {
+      districtName,
+      stateId,
+      cases,
+      cured,
+      active,
+      deaths,
+    } = request.body;
+    const updateDistrictQuery = `
+    UPDATE district
+    SET
+        district_name = '${districtName}',
+        state_id = ${stateId},
+        cases = ${cases},
+        cured = ${cured},
+        active = ${active},
+        deaths = ${deaths}
+    WHERE district_id = ${districtId};`;
+
+    await db.run(updateDistrictQuery);
+    response.send("district Details Updated");
+  }
+);
+
+///Get stats
+
+app.get(
+  "/states/:stateId/stats",
+  authenticateToken,
+  async (request, response) => {
+    const { stateId } = request.params;
+    const getStatsQuery = `
+    SELECT SUM(cases) AS totalCases,
+            SUM(cured) AS totalCured,
+            SUM(active) AS totalActive,
+            SUM(deaths) AS totalDeaths
+    FROM state LEFT JOIN district ON state.state_id = district.state_id
+    WHERE state.state_id = ${stateId};`;
+
+    const dbResponse = await db.get(getStatsQuery);
+    response.send(dbResponse);
+  }
+);
+
+module.exports = app;
